6fa59d766cbd12e5f18008116debb3119a478e50
Doug Metz <dwmetz@gmail.com>
2025-03-09 15:39:49 -0400

    Update main.rs

--- about/src/main.rs
+++ about/src/main.rs
@@ -63,6 +63,3 @@ fn main() {
     println!("{}", "  MalChela will prompt you to create them the first time you run the malware lookup function.".yellow());
     println!("{}", "\n".white());
 }
-
-
-

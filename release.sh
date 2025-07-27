3eba0e6f32d676e4da717b4068587376a93aadc5
Doug Metz <dwmetz@gmail.com>
2025-07-27 11:18:23 -0400

    Switch to /target/release path model for subtools; added release.sh for prebuild setup (v3.0.1)

---
+++ release.sh
@@ -0,0 +1,5 @@
+for tool in malchela fileminer mstrings hashit hashcheck mzcount mzhash xmzhash combine_yara strings_to_yara malhash nsrlquery extract_samples about fileanalyzer MalChelaGUI; do
+  echo "Building release for $tool..."
+  cargo build -j 1 --release -p "$tool"
+done
+

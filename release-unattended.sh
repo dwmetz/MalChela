0224a2ee913e10c5f202cfd8ce4d4787d40f4df8
Doug Metz <dwmetz@gmail.com>
2026-02-06 10:19:29 -0500

    Update  release-unattended.sh

--- release-unattended.sh
+++ release-unattended.sh
@@ -1,7 +1,7 @@
 
 echo "Defaulting to parallel build."
 echo "To run in throttled mode, run release.sh"
-
+BUILD_CMD="cargo build --release -p"
 for tool in malchela fileminer mstrings hashit hashcheck mzcount mzhash xmzhash combine_yara strings_to_yara malhash nsrlquery extract_samples about fileanalyzer MITRE_lookup MalChelaGUI; do
   echo "Building release for $tool..."
   $BUILD_CMD "$tool"

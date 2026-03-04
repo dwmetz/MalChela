0eb12ebd2440810be478b83aa6ff4dbf777bae9e
Doug Metz <dwmetz@gmail.com>
2026-03-04 13:02:05 -0500

    v3.1.1

--- MalChelaGUI/src/main.rs
+++ MalChelaGUI/src/main.rs
@@ -1233,7 +1233,7 @@ impl App for AppState {
 
         TopBottomPanel::top("top").show(ctx, |ui| {
             ui.horizontal(|ui| {
-                let mut title = "MalChela Analysis Toolkit v3.1.0".to_string();
+                let mut title = "MalChela Analysis Toolkit v3.1.1.".to_string();
                 if !self.edition.trim().is_empty() {
                     title.push_str(&format!(" ({})", self.edition));
                 }
